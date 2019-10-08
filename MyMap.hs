893da90342c6560c1895c2dfccc319da3770f7e4
m1Myp <55120919+m1Myp@users.noreply.github.com>
2019-10-08 22:25:41 +0700

    Update MyMap.hs

--- MyMap.hs
+++ MyMap.hs
@@ -1,9 +1,11 @@
 import Data.List
 
-reverseList :: [a] -> [a]
-reverseList [] = []
-reverseList (x : xs) = reverseList xs ++ [x] -- Не очень эффективно по памяти
+reverseList :: [Int] -> [Int]
+reverseList [] = [] 
+reverseList (x : xs) = reverseList xs ++ [x] -- Не очень эффективно по памяти(Простите)
 
+mymapleft :: (Int -> Int) -> [Int] -> [Int]
 mymapleft f xs = reverseList(foldl (\ys x -> f x : ys) [] xs)
 
+mymapright :: (Int -> Int) -> [Int] -> [Int]
 mymapright f xs = foldr (\x ys -> f x : ys) [] xs
