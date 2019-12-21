d5e9390fe8cddb42d61782dac67351ae3a9b1d08
m1Myp <55120919+m1Myp@users.noreply.github.com>
2019-12-21 17:55:14 +0700

    Last vers

--- lab2.hs
+++ lab2.hs
@@ -16,11 +16,17 @@ instance (Show a, Ord a, Eq a, Num a) => Eq (Complex a) where
  --Взято для длин, чтобы было логично с Ord. Тут еще должен быть sqrt, но как бы я не хочу флоатинг а, так шо не. Будем без корней.
 							
 instance (Show a, Ord a, Eq a, Num a) => Ord (Complex a) where
-	(>) (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = realP*realP + imagineP*imagineP > realP1*realP1 + imagineP1*imagineP1	
-	(<) (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = realP*realP + imagineP*imagineP <	realP1*realP1 + imagineP1*imagineP1				
-	(>=) (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = realP*realP + imagineP*imagineP <= realP1*realP1 + imagineP1*imagineP1	
-	(<=) (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = realP*realP + imagineP*imagineP <= realP1*realP1 + imagineP1*imagineP1	
+	compare  (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = compare (realP*realP + imagineP*imagineP) (realP1*realP1 + imagineP1*imagineP1)	
 --Как грится, ну это ж вектора. Мама мама я патернматчер
+
+instance (Show a, Ord a, Eq a, Num a) => Num (Complex a) where
+	(+) (ToComplex realP imagineP) (ToComplex realP1 imagineP1) = ToComplex (realP + realP1) (imagineP +imagineP1)
+	(*)	(ToComplex realP imagineP) (ToComplex realP1 imagineP1) = ToComplex (realP*realP1 - imagineP*imagineP1) (imagineP*realP1 + realP*imagineP1)
+	signum (ToComplex realP imagineP) | realP > 0 && imagineP > 0 = ToComplex 1 0
+									  | otherwise = ToComplex (-1) 0
+	fromInteger int = ToComplex (fromInteger int) 0
+	abs (ToComplex realP imagineP) = ToComplex (abs realP) (abs imagineP)
+	negate (ToComplex realP imagineP) = ToComplex (negate realP) (negate imagineP)
 
 data Quantum a = ToQuantum {
     complexP :: a,
@@ -34,10 +40,7 @@ instance (Show a, Ord a, Eq a, Num a) => Show (Quantum a) where
 	(show) (ToQuantum complexP stringP) = (show complexP) ++ " State: " ++ stringP
 
 instance (Show a, Ord a, Eq a, Num a) => Ord (Quantum a) where
-	(>) (ToQuantum complexP stringP) (ToQuantum complexP1 stringP1) = complexP > complexP1
-	(<) (ToQuantum complexP stringP) (ToQuantum complexP1 stringP1) = complexP < complexP1
-	(>=) (ToQuantum complexP stringP) (ToQuantum complexP1 stringP1) = complexP >= complexP1
-	(<=) (ToQuantum complexP stringP) (ToQuantum complexP1 stringP1) = complexP <= complexP1
+	compare (ToQuantum complexP stringP) (ToQuantum complexP1 stringP1) = compare complexP complexP1
 
 instance Functor Quantum where
 	fmap f (ToQuantum complexP stringP) = ToQuantum (f complexP) stringP
@@ -58,18 +61,12 @@ toPairList q = [(complexP, stringP) | (ToQuantum complexP stringP) <- q]
 
 fromPairList:: [(Complex a,String)] -> Qubit (Complex a)
 fromPairList pairList = [ToQuantum complexP stringP| (complexP,stringP) <- pairList]
-
+--фолдр
 scalarProduct::(Num a, Show a, Ord a, Eq a) => Qubit (Complex a) -> Qubit (Complex a) -> a
-scalarProduct [] [] = 0
-scalarProduct _ [] = 0
-scalarProduct [] _ = 0
-scalarProduct ((ToQuantum (ToComplex r1 i1) _):q1) ((ToQuantum (ToComplex r2 i2) _):q2) = (r1+r2)+(i1 + i2) + (scalarProduct q1 q2)
-
+scalarProduct q1 q2 = foldr(+) 0 [r1*r2 + i1*i2| ToQuantum (ToComplex r1 i1) _ <- q1, ToQuantum (ToComplex r2 i2) _ <- q2]
+--как и раньше
 entagle::(Num a, Show a, Ord a, Eq a) => Qubit (Complex a) ->Qubit (Complex a) ->Qubit (Complex a)
-entagle [] [] = []
-entagle _ [] = []
-entagle [] _ = []
-entagle ((ToQuantum (ToComplex r1 i1) str1):q1) ((ToQuantum (ToComplex r2 i2) str2):q2) = (ToQuantum (ToComplex (r1*r2-i1*i2) (r1*i2 + r2*i1)) (concat [str1,str2])) : (entagle q1 q2)
+entagle q1 q2 = [ToQuantum (complexP * complexP1) (stringP ++ stringP1) | (ToQuantum	 complexP stringP)<-q1 , (ToQuantum complexP1 stringP1) <- q2]
 
 l = ToQuantum (ToComplex 2 3) "1"
 k = ToQuantum (ToComplex 2 3) "1"
